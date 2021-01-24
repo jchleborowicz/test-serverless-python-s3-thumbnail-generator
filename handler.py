99e505cc9c5877eb2ae91d63eb3081274b4e1f12
Jacek Chleborowicz <j.chleborowicz@gmail.com>
2021-01-24 18:30:09 +0100

    Message.

--- handler.py
+++ handler.py
@@ -1,24 +1,58 @@
-import json
+import cStringIO
+import os
+from urllib import unquote_plus
+
+import boto3
+from PIL import Image, ImageOps
+
+s3 = boto3.client('s3')
+size = int(os.environ['THUMBNAIL_SIZE'])
 
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+def s3_thumbnail_generator(event, context):
+    print(event)
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = unquote_plus(event['Records'][0]['s3']['object']['key'])
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+    image = get_s3_image(bucket, key)
+    thumbnail = image_to_thumbnail(image)
+    thumbnail_key = new_filename(key)
+    url = upload_to_s3(bucket, thumbnail_key, thumbnail)
+    return url
 
-    return response
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+def get_s3_image(bucket, key):
+    print('Getting object: {}:{}'.format(bucket, key))
+    response = s3.get_object(Bucket=bucket, Key=key)
+    image_content = response['Body'].read()
+
+    file = cStringIO.StringIO(image_content)
+
+    return Image.open(file)
+
+
+def image_to_thumbnail(image):
+    return ImageOps.fit(image, (size, size), Image.ANTIALIAS)
+
+
+def new_filename(key):
+    key_split = key.rsplit('.', 1)
+    return 'thumb/' + key_split[0] + "_thumbnail.png"
+
+
+def upload_to_s3(bucket, key, image):
+    out_thumbnail = cStringIO.StringIO()
+    image.save(out_thumbnail, 'PNG')
+    out_thumbnail.seek(0)
+
+    response = s3.put_object(
+        ACL='public-read',
+        Body=out_thumbnail,
+        Bucket=bucket,
+        ContentType='image/png',
+        Key=key
+    )
+    print(response)
+
+    url = '{}/{}/{}'.format(s3.meta.endpoint_url, bucket, key)
+    return url
